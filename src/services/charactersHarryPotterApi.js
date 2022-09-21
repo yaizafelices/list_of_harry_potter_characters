1813ec4330b4e2f58b17741d734520d5bc3780e4
Yaiza Soria Felices <yaizafelices@gmail.com>
2022-09-21 22:18:26 +0200

    Change fetch

--- src/services/charactersHarryPotterApi.js
+++ src/services/charactersHarryPotterApi.js
@@ -1,6 +1,6 @@
 import { v4 as uuid } from 'uuid';
 const getDataApi = () => {
-    return fetch("http://hp-api.herokuapp.com/api/characters")
+    return fetch("https://hp-api.herokuapp.com/api/characters")
         .then(response => response.json())
         .then((data) => {
             const dataClean = data.map((character) => {
